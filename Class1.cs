15ab381ab5af1b209aa3d5eefdb9dfe916b69f3a
Systena <kawaradas@systena.co.jp>
2023-04-27 09:43:28 +0900

    commmit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Artifacts_test
+{
+    public class Class1
+    {
+        public static string Hello(string name)
+        {
+            return $"Hello {name} !";
+        }
+    }
+}
